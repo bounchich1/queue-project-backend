cf1e274e3b7a1df2db62d61538feb2ad589bd513
Воробьев Илья <ilya7050@gmail.com>
2023-12-29 17:21:49 +0700

    Implemented 3 API calls for queue
    complete_queue - deletes current_user and subject from queue
    
    add_to_queue - adds current user with specified subject to the queue
    
    get_queue - returns a list of json object ordered by time created and task number (from lower to higher) and filtered by subject number, which allows to have multiple queues in one db table
    
    TODO:
    1. Remove position field from InfoQueue db table
    2. Implement admin panel to manage different queues
    3. Pentest app and implement various safety measures

--- main.py
+++ main.py
@@ -1,28 +1,73 @@
 from fastapi import FastAPI, HTTPException, Depends, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from tortoise.contrib.fastapi import register_tortoise
-from models import User_Pydantic, UserIn_Pydantic, User
+from models import User_Pydantic, UserIn_Pydantic, User, InfoQueue_Pydantic, InfoQueueIn_Pydantic, InfoQueue
 from pydantic import BaseModel
 from starlette.exceptions import HTTPException
 from authentication import get_hashed_password, authenticate_user, create_access_token, get_current_user
-from typing import Annotated
+from typing import Annotated, List
 from datetime import datetime, timedelta
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 """
+
+TO DO:
+1. Add queue get/post requests with token verification
+2. Add dequeue post request with token verification
+
 {
   "first_name": "test2",
   "last_name": "user2",
   "email": "user2@email.com",
   "password": "213435fsdfsdf"
 }
+
+{
+  "first_name": "test_token",
+  "last_name": "user_token",
+  "email": "user_token@email.com",
+  "password": "123456"
+}
+{
+  "first_name": "test2_token",
+  "last_name": "user2_token",
+  "email": "user_token2@email.com",
+  "password": "123456"
+}
+{
+  "first_name": "test3_token",
+  "last_name": "user3_token",
+  "email": "user_token3@email.com",
+  "password": "123456"
+}
 """
+register_tortoise(
+    app,
+    db_url='sqlite://db.sqlite3',
+    modules={"models": ["models"]},
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
+
+origins = [
+    'http://localhost:5173',
+    'http://localhost:5173/',
+    'http://localhost:5173/registration',
+    'http://localhost:5173/dashboard',
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class Token(BaseModel):
     access_token: str
-    token_type: str
 
 
 class Status(BaseModel):
@@ -31,14 +76,26 @@ class Status(BaseModel):
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "API"}
 
 
-@app.post("/registration", response_model=User_Pydantic)
+@app.post("/registration", response_model=Token)
 async def create_user(user: UserIn_Pydantic):
-    user.password = get_hashed_password(user.password)
+    plain_password = user.password
+    user.password = await get_hashed_password(user.password)
     user_obj = await User.create(**user.model_dump(exclude_unset=True))
-    return await User_Pydantic.from_tortoise_orm(user_obj)
+    user = await authenticate_user(user_obj.email, plain_password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.email}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
 
 
 @app.get("/user/{user_id}", response_model=User_Pydantic)
@@ -69,7 +126,7 @@ async def login_for_access_token(
     access_token = create_access_token(
         data={"sub": user.email}, expires_delta=access_token_expires
     )
-    return {"access_token": access_token, "token_type": "bearer"}
+    return {'access_token': access_token}
 
 
 @app.get("/users/me/", response_model=User_Pydantic)
@@ -77,10 +134,27 @@ async def read_users_me(current_user: User_Pydantic = Depends(get_current_user))
     return current_user
 
 
-register_tortoise(
-    app,
-    db_url='sqlite://db.sqlite3',
-    modules={"models": ["models"]},
-    generate_schemas=True,
-    add_exception_handlers=True,
-)
+@app.post("/infoqueue/add_to_queue/", response_model=List[InfoQueue_Pydantic])
+async def add_to_queue(remaining_inf: InfoQueueIn_Pydantic, current_user: User_Pydantic = Depends(get_current_user)):
+    user_fname = current_user.first_name
+    user_lname = current_user.last_name
+    position = await InfoQueue.all().count() + 1
+    task = remaining_inf.task_number
+    subj = remaining_inf.subject_number
+    await InfoQueue.create(position=position, first_name=user_fname, last_name=user_lname,
+                           task_number=task, subject_number=subj)
+    return await InfoQueue.all().order_by('task_number', 'created_at').filter(subject_number=subj).values()
+
+
+@app.get('/infoqueue/get_queue/{subject}', response_model=List[InfoQueue_Pydantic])
+async def get_queue(subject: int, current_user: User_Pydantic = Depends(get_current_user)):
+    return await InfoQueue.all().order_by('task_number', 'created_at').filter(subject_number=subject).values()
+
+
+@app.delete('/infoqueue/complete/{subject}', response_model=InfoQueue_Pydantic)
+async def complete_queue(subject: int, current_user: User_Pydantic = Depends(get_current_user)):
+    current_last_name = current_user.last_name
+    end_queue = await InfoQueue.filter(last_name=current_last_name, subject_number=subject)
+    if not end_queue:
+        raise HTTPException(status_code=404, detail=f"{current_last_name} not found")
+    return await InfoQueue.all().order_by('task_number', 'created_at').filter(subject_number=subject).values()
--- models.py
+++ models.py
@@ -15,17 +15,24 @@ class User(models.Model):
 
 
 class InfoQueue(models.Model):
-    id = fields.IntField(pk=True)
-    queued_person = fields.CharField(max_length=255, null=True)
+    position = fields.IntField()
+    first_name = fields.CharField(max_length=100, null=True)
+    last_name = fields.CharField(max_length=100, null=True)
     task_number = fields.IntField()
+    subject_number = fields.IntField()
     created_at = fields.DatetimeField(auto_now_add=True)
     modified_at = fields.DatetimeField(auto_now=True)
 
     class PydanticMeta:
-        exclude = ['created_at']
+        exclude = []
 
 
 User_Pydantic = pydantic_model_creator(User, name="User")
 UserIn_Pydantic = pydantic_model_creator(User, name="UserIn", exclude_readonly=True, exclude=('created_at',))
 InfoQueue_Pydantic = pydantic_model_creator(InfoQueue, name='InfoQueue')
-InfoQueueIn_Pydantic = pydantic_model_creator(InfoQueue, name="InfoQueueIn", exclude_readonly=True)
+InfoQueueIn_Pydantic = pydantic_model_creator(InfoQueue, name="InfoQueueIn", exclude_readonly=True,
+                                              exclude=('created_at',
+                                                       'modified_at',
+                                                       'first_name',
+                                                       'last_name',
+                                                       'position'))
