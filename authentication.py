d560041af7c27e8f22bc1dada5d3639cc9d49c8d
Воробьев Илья <ilya7050@gmail.com>
2024-01-26 14:29:08 +0700

    Started implementing an email verification
    TODO:
    1. Remove position field from InfoQueue db table
    2. Implement admin panel to manage different queues
    3. Pentest app and implement various safety measures

--- authentication.py
+++ authentication.py
@@ -60,6 +60,17 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     return encoded_jwt
 
 
+def create_verification_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_verify_jwt = jwt.encode(to_encode, secret_key, algorithm=algorithm)
+    return encoded_verify_jwt
+
+
 async def get_current_user(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
@@ -77,6 +88,3 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
     if user is None:
         raise credentials_exception
     return user
-
-
-
