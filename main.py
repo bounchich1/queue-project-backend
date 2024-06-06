40a616d06e9994ca0491215cc8f8d165b475b233
Воробьев Илья <ilya7050@gmail.com>
2024-06-06 20:42:12 +0700

    Server-host ready (probably)

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from tortoise.contrib.fastapi import register_tortoise
 from models import User_Pydantic, UserIn_Pydantic, User, InfoQueue_Pydantic, InfoQueueIn_Pydantic, InfoQueue, \
     Subjects_Pydantic, SubjectsIn_Pydantic, Subjects, Tokens_Pydantic, Tokens, TokensIn_Pydantic, Subscription, \
-    Subscription_Pydantic, SubscriptionIn_Pydantic, UserOut_Pydantic
+    Subscription_Pydantic, SubscriptionIn_Pydantic, UserOut_Pydantic, Groups, Groups_Pydantic, GroupsIn_Pydantic, \
+    SubjectsUpd_Pydantic
 from pydantic import BaseModel
 from fastapi_mail import FastMail, MessageSchema, ConnectionConfig, MessageType
 from starlette.exceptions import HTTPException
@@ -118,17 +119,21 @@ async def create_user(user: UserIn_Pydantic):
     access_token = create_access_token(
         data={"sub": user.email}, expires_delta=access_token_expires
     )
+    """
     verification_token_expires = timedelta(minutes=10)
     verification_token = create_access_token(data={'sub': user.email}, expires_delta=verification_token_expires)
     await send_verification_email(user.email, verification_token)
+    """
     return {"access_token": access_token, "token_type": "bearer"}
 
 
+"""
 async def send_verification_email(email: str, token: str):
     message = MessageSchema(subject="Verify your email", recipients=[email],
                             body=f"Click the link below to verify your email token: /{token}",
                             subtype=MessageType.html)
     await fast_mail.send_message(message)
+"""
 
 
 @app.get("/user/{user_id}", response_model=User_Pydantic)
@@ -169,39 +174,34 @@ async def read_users_me(current_user: User_Pydantic = Depends(get_current_user))
 async def add_to_queue(remaining_inf: InfoQueueIn_Pydantic, current_user: User_Pydantic = Depends(get_current_user)):
     current_id = current_user.id
     subj = remaining_inf.subject_number
-    current_group = current_user.group_number
-    position = await InfoQueue.all().order_by('position').filter(subject_number=subj,
-                                                                 group_number=current_group).count()
-    if not position:
-        position = 1
-    else:
-        position += 1
-    existing_entry = await InfoQueue.filter(user_id=current_id, subject_number=subj, group_number=current_group).first()
+    current_group = current_user.group_id
+    position = 1
+    existing_entry = await InfoQueue.filter(user_id=current_id, subject_number=subj, group_id=current_group).first()
     if existing_entry:
         raise HTTPException(status_code=400, detail='User is already in queue')
     task = remaining_inf.task_number
     user_fname = current_user.first_name
     user_lname = current_user.last_name
-    await InfoQueue.create(user_id=current_id, position=position, group_number=current_group, first_name=user_fname,
+    await InfoQueue.create(user_id=current_id, position=position, group_id=current_group, first_name=user_fname,
                            last_name=user_lname,
                            task_number=task, subject_number=subj)
-    return await InfoQueue.all().order_by('position',
+    return await InfoQueue.all().order_by('created_at',
                                           'task_number').filter(subject_number=subj,
-                                                                group_number=current_group).only('position',
-                                                                                                 'task_number',
-                                                                                                 'first_name',
-                                                                                                 'last_name').values()
+                                                                group_id=current_group).only('position',
+                                                                                             'task_number',
+                                                                                             'first_name',
+                                                                                             'last_name').values()
 
 
 @app.get('/infoqueue/get_queue/{subject}', response_model=List[InfoQueue_Pydantic])
 async def get_queue(subject: str, current_user: User_Pydantic = Depends(get_current_user)):
-    return await InfoQueue.all().order_by('position',
+    return await InfoQueue.all().order_by('created_at',
                                           'task_number'). \
         filter(subject_number=subject,
-               group_number=current_user.group_number).only('position',
-                                                            'task_number',
-                                                            'first_name',
-                                                            'last_name').values()
+               group_id=current_user.group_id).only('position',
+                                                    'task_number',
+                                                    'first_name',
+                                                    'last_name').values()
 
 
 @app.delete('/infoqueue/complete/{subject}', response_model=List[InfoQueue_Pydantic])
@@ -212,37 +212,45 @@ async def complete_queue(subject: str, current_user: User_Pydantic = Depends(get
                                        subject_number=subject).delete()
     if not end_queue:
         raise HTTPException(status_code=404, detail=f"{current_last_name} not found")
-    return await InfoQueue.all().order_by('position',
-                                          'task_number'). \
+    return await InfoQueue.all().order_by('position'). \
         filter(subject_number=subject,
-               group_number=current_user.group_number).only('position',
-                                                            'task_number',
-                                                            'first_name',
-                                                            'last_name').values()
+               group_id=current_user.group_id).only('position',
+                                                    'task_number',
+                                                    'first_name',
+                                                    'last_name').values()
 
 
 @app.get('/infoqueue/get_subjects/', response_model=List[Subjects_Pydantic])
 async def get_subjects(current_user: User_Pydantic = Depends(get_current_user)):
     return await Subjects.all().order_by('subject_full_name'). \
-        filter(group_number=current_user.group_number).only('subject_full_name',
-                                                            'subject_short_name').values()
+        filter(group_id=current_user.group_id).only('subject_full_name',
+                                                    'subject_short_name').values()
 
 
 @app.post('/infoqueue/add_new_subjects/', response_model=List[Subjects_Pydantic])
 async def add_new_subjects(subjects_info: SubjectsIn_Pydantic,
                            current_user: User_Pydantic = Depends(get_current_user)):
-    current_group = current_user.group_number
+    current_group = current_user.group_id
     subjects_full_name = subjects_info.subject_full_name
     subjects_short_name = subjects_info.subject_short_name
-    existing_subject = await Subjects.filter(group_number=current_group, subject_full_name=subjects_full_name,
+    existing_subject = await Subjects.filter(group_id=current_group, subject_full_name=subjects_full_name,
                                              subject_short_name=subjects_short_name)
     if existing_subject:
         raise HTTPException(status_code=400, detail=f'{subjects_full_name} already exists')
     else:
-        await Subjects.create(group_number=current_group, subject_full_name=subjects_full_name,
+        await Subjects.create(group_id=current_group, subject_full_name=subjects_full_name,
                               subject_short_name=subjects_short_name)
-    return await Subjects.all().filter(group_number=current_group).only('subject_full_name',
-                                                                        'subject_short_name').values()
+    return await Subjects.all().filter(group_id=current_group).only('subject_full_name',
+                                                                    'subject_short_name').values()
+
+
+@app.post('/infoqueue/update_subject', response_model=Status)
+async def update_subject(subject_upd: SubjectsUpd_Pydantic, current_user: User_Pydantic = Depends(get_current_user)):
+    current_subject = await Subjects.get(id=subject_upd.id)
+    current_subject.subject_short_name = subject_upd.subject_short_name
+    current_subject.subject_full_name = subject_upd.subject_full_name
+    await current_subject.save()
+    return Status(message='Предмет был успешно обновлен')
 
 
 @app.get('/user/subscription_plan/', response_model=Subscription_Pydantic)
@@ -254,46 +262,79 @@ async def get_subscription_plan(current_user: User_Pydantic = Depends(get_curren
 
 
 @app.get('/user/generate_invitation_token/', response_model=Tokens_Pydantic)
-async def get_invitation_token(current_user: User_Pydantic = Depends(get_current_user)):
+async def create_invitation_token(current_user: User_Pydantic = Depends(get_current_user)):
     subscription_plan = await Subscription_Pydantic.from_queryset_single(Subscription.get(owner_id=current_user.id))
     new_token = generate_invitation_token()
-    await Tokens.create(group_number=subscription_plan.group_number,
+    await Tokens.create(group_id=subscription_plan.group_id,
                         remaining_activations=subscription_plan.group_population,
-                        token=new_token, owner_id=subscription_plan.owner_id)
-    return await Tokens_Pydantic.from_queryset_single(Tokens.get(group_number=subscription_plan.group_number))
+                        token=new_token, owner_id=subscription_plan.owner_id, expires=subscription_plan.expires)
+    return await Tokens_Pydantic.from_queryset_single(Tokens.get(group_id=subscription_plan.group_id))
+
+
+@app.get('/user/get_invitation_token/', response_model=Tokens_Pydantic)
+async def get_invitation_token(current_user: User_Pydantic = Depends(get_current_user)):
+    return await Tokens_Pydantic.from_queryset_single(Tokens.get(group_id=current_user.group_id))
 
 
 @app.post('/user/activate_subscription/', response_model=Status)
-async def activate_subscription(subscription_info: SubscriptionIn_Pydantic,
+async def activate_subscription(subscription_info: SubscriptionIn_Pydantic, group_info: GroupsIn_Pydantic,
                                 current_user: User_Pydantic = Depends(get_current_user)):
-    existing_subscription = await Subscription.filter(owner_id=current_user.id)
+    if current_user.group_id is None:
+        new_group = await Groups.create(group_number=group_info.group_number)
+        user = await User.get(id=current_user.id)
+        user.group_id = new_group.group_id
+        await user.save()
+    existing_subscription = await Subscription.filter(owner_id=current_user.group_id)
     if existing_subscription:
         raise HTTPException(status_code=400, detail='Ваша подписка уже активна')
     current_datetime = datetime.now()
     expires = current_datetime + relativedelta(months=+subscription_info.months)
+    user = await User.get(id=current_user.id)
     await Subscription.create(tier=subscription_info.tier, owner_id=current_user.id,
                               group_population=subscription_info.group_population,
-                              expires=expires, created_at=current_datetime, group_number=subscription_info.group_number,
+                              expires=expires, created_at=current_datetime, group_id=user.group_id,
                               months=subscription_info.months)
-    if current_user.group_number is None:
-        user = await User.get(id=current_user.id)
-        user.group_number = subscription_info.group_number
-        await user.save()
-
+    user = await User.get(id=current_user.id)
+    user.role = 'moderator'
+    user.subscription_expires = expires
+    await user.save()
     return Status(message='Ваша подписка успешно активирована')
 
 
 @app.post('/user/enter_invitation_token/{token}', response_model=Status)
-async def enter_invitation_token(token, current_user: User_Pydantic = Depends(get_current_user)):
+async def enter_invitation_token(token: str, current_user: User_Pydantic = Depends(get_current_user)):
     token_info = await Tokens.get(token=token)
-    if current_user.group_number is None:
-        user = await User.get(id=current_user.id)
-        user.group_number = token_info.group_number
-        await user.save()
+    current_time = datetime.now()
+    user = await User.get(id=current_user.id)
     if token_info.remaining_activations == 0:
         raise HTTPException(status_code=400, detail='Превышен лимит активаций!')
     else:
+        if current_user.group_id is None:
+            user.group_id = token_info.group_id
+            user.subscription_expires = token_info.expires
+            await user.save()
         token_info.remaining_activations -= 1
         await token_info.save()
-    return Status(message=f'Вы успешно добавлены в группу {token_info.group_number}, ваша подписка активна')
 
+    return Status(message=f'Вы успешно добавлены в группу {token_info.group_id}, ваша подписка активна')
+
+
+@app.post('/groups/set_group_name/', response_model=Groups_Pydantic)
+async def set_group_number(group_info: GroupsIn_Pydantic, current_user: User_Pydantic = Depends(get_current_user)):
+    new_group_number = group_info.group_number
+    new_group = await Groups.create(group_number=new_group_number)
+    if current_user.group_id is None:
+        user = await User.get(id=current_user.id)
+        user.group_id = new_group.group_id
+        await user.save()
+    return await Groups.get(group_id=new_group.group_id)
+
+
+@app.get('/users/list_of_users/', response_model=List[User_Pydantic])
+async def get_list_of_users(current_user: User_Pydantic = Depends(get_current_user)):
+    return await User.filter(group_id=current_user.group_id).only('first_name', 'last_name').values()
+
+
+@app.get('/groups/get_group_number/', response_model=Groups_Pydantic)
+async def get_group_number(current_user: User_Pydantic = Depends(get_current_user)):
+    return await Groups.get(group_id=current_user.group_id)
--- models.py
+++ models.py
@@ -6,11 +6,12 @@ class User(models.Model):
     id = fields.UUIDField(pk=True, auto_generate=True)
     first_name = fields.CharField(max_length=100, null=True)
     last_name = fields.CharField(max_length=100, null=True)
-    group_number = fields.CharField(max_length=15, null=True)
+    group_id = fields.UUIDField(null=True)
     email = fields.CharField(max_length=150, unique=True, null=True)
     password = fields.CharField(max_length=60, null=True)
     created_at = fields.DatetimeField(auto_now_add=True)
     subscription_expires = fields.DatetimeField(null=True)
+    role = fields.CharField(max_length=50, null=False, default="user")
 
     class Meta:
         exclude = ["password"]
@@ -19,7 +20,7 @@ class User(models.Model):
 class InfoQueue(models.Model):
     user_id = fields.UUIDField()
     position = fields.IntField()
-    group_number = fields.CharField(max_length=15, null=True)
+    group_id = fields.UUIDField()
     first_name = fields.CharField(max_length=100, null=True)
     last_name = fields.CharField(max_length=100, null=True)
     task_number = fields.IntField()
@@ -33,7 +34,7 @@ class InfoQueue(models.Model):
 
 class Subjects(models.Model):
     id = fields.UUIDField(pk=True, auto_generate=True)
-    group_number = fields.CharField(max_length=15, null=True)
+    group_id = fields.UUIDField()
     subject_full_name = fields.CharField(max_length=155, null=True)
     subject_short_name = fields.CharField(max_length=15, null=True)
 
@@ -41,7 +42,8 @@ class Subjects(models.Model):
 class Tokens(models.Model):
     token = fields.CharField(max_length=100, null=False)
     remaining_activations = fields.IntField(null=False)
-    group_number = fields.CharField(max_length=15, null=False)
+    group_id = fields.UUIDField()
+    expires = fields.DatetimeField(null=False)
     owner_id = fields.UUIDField()
 
 
@@ -50,16 +52,22 @@ class Subscription(models.Model):
     created_at = fields.DatetimeField()
     months = fields.IntField(null=False)
     group_population = fields.IntField(null=False)
-    group_number = fields.CharField(null=False, max_length=15)
+    group_id = fields.UUIDField()
     owner_id = fields.UUIDField()
     tier = fields.IntField(null=False)
+
+
+class Groups(models.Model):
+    group_number = fields.CharField(null=False, max_length=15)
+    group_id = fields.UUIDField(pk=True, auto_generate=True)
 
 
 User_Pydantic = pydantic_model_creator(User, name="User")
 UserOut_Pydantic = pydantic_model_creator(User, name='UserOut', exclude=('password', 'created_at'))
 UserIn_Pydantic = pydantic_model_creator(User, name="UserIn", exclude_readonly=True, exclude=('created_at',
-                                                                                              'group_number',
-                                                                                              'subscription_expires'))
+                                                                                              'group_id',
+                                                                                              'subscription_expires',
+                                                                                              'role'))
 InfoQueue_Pydantic = pydantic_model_creator(InfoQueue, name='InfoQueue')
 InfoQueueIn_Pydantic = pydantic_model_creator(InfoQueue, name="InfoQueueIn", exclude_readonly=True,
                                               exclude=('created_at',
@@ -68,15 +76,18 @@ InfoQueueIn_Pydantic = pydantic_model_creator(InfoQueue, name="InfoQueueIn", exc
                                                        'last_name',
                                                        'position',
                                                        'user_id',
-                                                       'group_number',))
+                                                       'group_id',))
 Subjects_Pydantic = pydantic_model_creator(Subjects, name="Subjects")
 SubjectsIn_Pydantic = pydantic_model_creator(Subjects, name="SubjectsIn", exclude_readonly=True,
-                                             exclude=('group_number', 'id'))
-
+                                             exclude=('group_id', 'id'))
+SubjectsUpd_Pydantic = pydantic_model_creator(Subjects, name="SubjectsUpd", exclude=('group_id',))
 Tokens_Pydantic = pydantic_model_creator(Tokens, name="Tokens")
 TokensIn_Pydantic = pydantic_model_creator(Tokens, name="TokensIn", exclude_readonly=True)
 
 
 Subscription_Pydantic = pydantic_model_creator(Subscription, name="Subscription")
 SubscriptionIn_Pydantic = pydantic_model_creator(Subscription, name="SubscriptionIn", exclude_readonly=True,
-                                                 exclude=('created_at', 'owner_id', 'expires'))
+                                                 exclude=('created_at', 'owner_id', 'expires', 'group_id'))
+
+Groups_Pydantic = pydantic_model_creator(Groups, name="Groups")
+GroupsIn_Pydantic = pydantic_model_creator(Groups, name="GroupsIn", exclude_readonly=True, exclude=('group_id',))
